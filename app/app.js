fede9493f7a1988a3290af1909b7079c568dc3dc
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2017-11-12 11:08:28 +1300

    Render list of UFO sightings

--- app/app.js
+++ app/app.js
@@ -1,4 +1,17 @@
-function init () {
+async function init () {
+  const res = await fetch('http://localhost:9292?page=1&per_page=100')
+  const json = await res.json()
+  renderSightings(json.data)
+}
+
+function renderSightings(data) {
+  const list = document.querySelector('#sightings-list')
+  const items = data.map(sighting => {
+    const li = document.createElement('li')
+    li.innerText = `${sighting['Date_time']}\t${sighting['description']}`
+    return li
+  })
+  items.forEach(item => list.appendChild(item))
 }
 
 window.onload = init
--- config.ru
+++ config.ru
@@ -1,2 +1,11 @@
 require 'server'
+require 'rack/cors'
+
+use Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', :headers => :any, :methods => :get
+  end
+end
+
 run Server
