06fa8eab26a3a7ef062aa837521bc26bfc775565
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2017-11-12 11:42:03 +1300

    Add loading spinner

--- app/app.js
+++ app/app.js
@@ -1,15 +1,18 @@
 const USE_WORKER = true
 
 async function init () {
+  showSpinner()
   if (USE_WORKER) {
     const worker = new Worker('app/worker.js')
     worker.onmessage = function (e) {
       renderSightings(e.data)
+      hideSpinner()
     }
     worker.postMessage('fetch')
   } else {
     const res = await UFOSightings.getData()
     renderSightings(res.data)
+    hideSpinner()
   }
 }
 
@@ -21,6 +24,16 @@ function renderSightings(data) {
     return li
   })
   items.forEach(item => list.appendChild(item))
+}
+
+function showSpinner () {
+  const spinner = document.querySelector('.loading-spinner')
+  spinner.classList.remove('hidden')
+}
+
+function hideSpinner () {
+  const spinner = document.querySelector('.loading-spinner')
+  spinner.classList.add('hidden')
 }
 
 window.onload = init
