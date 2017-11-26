96f54088dd7d847f45af3d0951ea36489b4dd01d
Luke McFarlane <luke.mcfarlane@trineo.co.nz>
2017-11-26 14:38:09 +1300

    Separate out rendering code

--- app/app.js
+++ app/app.js
@@ -1,12 +1,19 @@
 import './nouislider.min.js'
+import {
+  renderElapsedTime,
+  renderSightings,
+  renderSlider,
+  showSpinner,
+  hideSpinner
+} from './renderers.js'
 
 const USE_WORKER = false
 
 function init () {
-  renderSlider()
+  renderSlider(loadSightings)
 }
 
-async function loadSightings (fromYear, toYear) {
+async function loadSightings ({ fromYear, toYear }) {
   showSpinner()
   const startedAt = new Date()
   const params = {fromYear, toYear}
@@ -24,60 +31,6 @@ async function loadSightings (fromYear, toYear) {
     renderElapsedTime(res.data.length, startedAt)
     hideSpinner()
   }
-}
-
-function renderElapsedTime (num, startedAt) {
-  const el = document.querySelector('.elapsed-time')
-  const secs = ((new Date()) - startedAt) / 1000
-  el.innerText = `Loaded ${num} sightings in ${secs} seconds`
-}
-
-function renderSightings(data) {
-  const list = document.querySelector('#sightings-list')
-  const items = data.map(sighting => {
-    const li = document.createElement('li')
-    li.innerText = `${sighting['Date_time']}\t${sighting['description']}`
-    return li
-  })
-  items.forEach(item => list.appendChild(item))
-}
-
-function renderSlider () {
-  const slider = document.getElementById('year-slider')
-
-  noUiSlider.create(slider, {
-    start: [1980, 1990],
-    step: 1,
-    tooltips: true,
-    connect: true,
-    range: {
-      min: 1900,
-      max: 2017
-    },
-    format: {
-      to (value) {
-        return parseInt(value)
-      },
-      from (value) {
-        return value
-      }
-    }
-  })
-
-  slider.noUiSlider.on('set', e => {
-    const [fromYear, toYear] = e
-    loadSightings(fromYear, toYear)
-  })
-}
-
-function showSpinner () {
-  const spinner = document.querySelector('.loading-spinner')
-  spinner.classList.remove('hidden')
-}
-
-function hideSpinner () {
-  const spinner = document.querySelector('.loading-spinner')
-  spinner.classList.add('hidden')
 }
 
 window.onload = init
---
+++ app/renderers.js
@@ -0,0 +1,53 @@
+export function renderElapsedTime (num, startedAt) {
+  const el = document.querySelector('.elapsed-time')
+  const secs = ((new Date()) - startedAt) / 1000
+  el.innerText = `Loaded ${num} sightings in ${secs} seconds`
+}
+
+export function renderSightings(data) {
+  const list = document.querySelector('#sightings-list')
+  const items = data.map(sighting => {
+    const li = document.createElement('li')
+    li.innerText = `${sighting['Date_time']}\t${sighting['description']}`
+    return li
+  })
+  items.forEach(item => list.appendChild(item))
+}
+
+export function renderSlider (onChange) {
+  const slider = document.getElementById('year-slider')
+
+  noUiSlider.create(slider, {
+    start: [1980, 1990],
+    step: 1,
+    tooltips: true,
+    connect: true,
+    range: {
+      min: 1900,
+      max: 2017
+    },
+    format: {
+      to (value) {
+        return parseInt(value)
+      },
+      from (value) {
+        return value
+      }
+    }
+  })
+
+  slider.noUiSlider.on('set', e => {
+    const [fromYear, toYear] = e
+    onChange({ fromYear, toYear })
+  })
+}
+
+export function showSpinner () {
+  const spinner = document.querySelector('.loading-spinner')
+  spinner.classList.remove('hidden')
+}
+
+export function hideSpinner () {
+  const spinner = document.querySelector('.loading-spinner')
+  spinner.classList.add('hidden')
+}
